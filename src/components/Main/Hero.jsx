e2726434c3e96c74b5524083f8e3dbf040f374c0
Dan <daniel.speranskiy@live.com>
2024-04-08 08:01:26 +0400

    fixed the hero sizing on all devices

--- src/components/Main/Hero.jsx
+++ src/components/Main/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="relative">
       <div className="absolute inset-0 ">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 max-w-md">
           <div>
             <h1 className="text-sm md:text-lg font-light">Meet With</h1>
             <h2 className="text-5xl md:text-8xl font-[300]">Our Best</h2>
@@ -16,7 +16,7 @@ const Hero = () => {
               Online
             </h4>
           </div>
-          <div className="mr-60 md:mr-0">
+          <div className="max-w-[16rem] md:max-w-xl z-[5]">
             <p className="text-sm md:text-lg font-light leading-tight">
               Connect with millions of doctors online. Chat with them and get a
               second opinion.All in one place. Stop waiting. Start treating.
@@ -28,16 +28,16 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="absolute top-0 left-0 w-full">
-        <div className="w-full h-full flex justify-center items-center overflow-hidden md:overflow-visible">
+      <div className="absolute top-0 md:top-0 left-6 md:left-0 w-full">
+        <div className="w-[110%] md:w-full h-full flex justify-center items-center overflow-hidden md:overflow-visible">
           <div className="relative ">
-            <div className="absolute top-[-100px] right-0 lg:-right-40 w-1/2 h-[130%] md:h-[117.6%] custom:h-[115%] lg:h-[114%]  bg-gradient-to-r from-blue-300/70 to-transparent skew-x-[-3deg]"></div>
+            <div className="absolute top-[-70px] md:top-[-100px] -right-5 lg:-right-40 w-1/2 h-[130%] md:h-[117.6%] custom:h-[115%] lg:h-[114%]  bg-gradient-to-r from-blue-300/70 to-transparent skew-x-[-3deg]"></div>
             <Image
               src="/hero.png"
               alt="Hero"
               width={1000}
               height={1000}
-              className="relative ml-20 lg:ml-40 xl:ml-60 2xl:ml-80"
+              className="relative ml-28 lg:ml-40 xl:ml-60 2xl:ml-80"
             />
           </div>
         </div>
